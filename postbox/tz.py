9cbc2a583d745b10cbafaa18afc99776c5ec534f
jyothsna boyina <jyothsna boyina>
2016-07-13 14:46:46 +0530

    updated tz.py

--- postbox/tz.py
+++ postbox/tz.py
@@ -7,4 +7,4 @@ from django.utils import timezone
 class TimezoneMiddleware(object):
     def process_request(self, request):
         tz=str(tzlocal.get_localzone().zone)
-        timezone.activate(pytz.timezone(tz))
+        timezone.activate(pytz.timezone('Asia/Calcutta'))
